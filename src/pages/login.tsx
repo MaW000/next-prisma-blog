e424cb3ba65da2e15145bd783eb1f9c1602546f2
matt wardlow <mwardlow0@gmail.com>
2022-09-06 23:41:30 -0400

    added jwt auth

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,41 +1,21 @@
-import React, { useState } from 'react';
-import Link from 'next/link';
-import { trpc } from '../utils/trpc'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { CreateUserInput } from '../schema/user.schema';
-import { useRouter } from 'next/router';
+import dynamic from 'next/dynamic'
+import { CreateUserInput } from '../schema/user.schema'
+import { trpc } from '../utils/trpc'
 
-function register() {
-    const { handleSubmit, register } = useForm<CreateUserInput>()
-    const [success, setSuccess] = useState(false)
-    const router = useRouter()
-    const { mutate, error } = trpc.useMutation(['users.request-otp'], {
-        onSuccess: () => {
-            setSuccess(true)
-        }
-    })
-    
-    function onSubmit(values: CreateUserInput) {
-        mutate(values)
-    }
+const LoginForm = dynamic(() => import('../components/loginform'), {
+  ssr: false,
+})
 
-    return (
-    <>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            {error && error.message}
-            {success && <p>Check your email</p>}
-        <h1>Login</h1>
-        <input 
-            type="email" 
-            placeholder='jane.doe@example.com' 
-            {...register('email')} 
-        />
-        <button type='submit'>Login</button>
-        </form>
-
-        <Link href="/login">Register</Link>
-    </>
+function LoginPage() {
+  return (
+    <div>
+      <LoginForm />
+    </div>
   )
 }
 
-export default register
+export default LoginPage
--- src/server/route/user.router.ts
+++ src/server/route/user.router.ts
@@ -1,80 +1,129 @@
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime'
-import { createRouter } from "../createRouter";
 import * as trpc from '@trpc/server'
+import { serialize } from 'cookie'
+import { resolve } from 'path'
+import { baseurl, url } from '../../constants'
 import {
-    createUserSchema,
-    createUserOutputSchema,
-    requestOtpSchema
+  createUserOutputSchema,
+  createUserSchema,
+  requestOtpSchema,
+  verifyOtpSchema,
 } from '../../schema/user.schema'
-import { sendLoginEmail } from '../../utils/mailer';
 import { decode, encode } from '../../utils/base64'
- import { url } from '../../constants'
+import { signJwt } from '../../utils/jwt'
+import { sendLoginEmail } from '../../utils/mailer'
+import { createRouter } from '../createRouter'
+
 export const userRouter = createRouter()
-.mutation('register-user', {
-  input: createUserSchema,
-  async resolve({ ctx, input }) {
-    const { email, name } = input
-    console.log('dog')
-    try {
-      const user = await ctx.prisma.user.create({
-        data: {
+  .mutation('register-user', {
+    input: createUserSchema,
+    async resolve({ ctx, input }) {
+      const { email, name } = input
+
+      try {
+        const user = await ctx.prisma.user.create({
+          data: {
+            email,
+            name,
+          },
+        })
+
+        return user
+      } catch (e) {
+        if (e instanceof PrismaClientKnownRequestError) {
+          if (e.code === 'P2002') {
+            throw new trpc.TRPCError({
+              code: 'CONFLICT',
+              message: 'User already exists',
+            })
+          }
+        }
+
+        throw new trpc.TRPCError({
+          code: 'INTERNAL_SERVER_ERROR',
+          message: 'Something went wrong',
+        })
+      }
+    },
+  })
+  .mutation('request-otp', {
+    input: requestOtpSchema,
+    async resolve({ input, ctx }) {
+      const { email, redirect } = input
+
+      const user = await ctx.prisma.user.findUnique({
+        where: {
           email,
-          name,
         },
       })
 
-      return user
-    } catch (e) {
-      if (e instanceof PrismaClientKnownRequestError) {
-        if (e.code === 'P2002') {
-          throw new trpc.TRPCError({
-            code: 'CONFLICT',
-            message: 'User already exists',
-          })
-        }
+      if (!user) {
+        throw new trpc.TRPCError({
+          code: 'NOT_FOUND',
+          message: 'User not found',
+        })
       }
 
-      throw new trpc.TRPCError({
-        code: 'INTERNAL_SERVER_ERROR',
-        message: 'Something went wrong',
-      })
-    }
-  },
-}).mutation('request-otp', {
-  input: requestOtpSchema,
-  async resolve({input, ctx}) {
-    const {email, redirect} = input
-
-    const user = await ctx.prisma.user.findUnique({
-      where: {
-        email,
-      },
-    })
-
-    if(!user) {
-      throw new trpc.TRPCError({
-        code: 'NOT_FOUND',
-        message: 'User not found'
-      })
-    }
-
-    const token = await ctx.prisma.loginToken.create({
-      data: {
-        redirect,
-        user: {
-          connect: {
-            id: user.id,
+      const token = await ctx.prisma.loginToken.create({
+        data: {
+          redirect,
+          user: {
+            connect: {
+              id: user.id,
+            },
           },
         },
-      },
-    })
+      })
+      // send email to user
+      sendLoginEmail({
+        token: encode(`${token.id}:${user.email}`),
+        url: baseurl,
+        email: user.email,
+      })
 
-    await sendLoginEmail({
-      token: encode(`${token.id}:${user.email}`),
-      url: url,
-      email: user.email
-    })
+      return true
+    },
+  })
+  .query('verify-otp', {
+    input: verifyOtpSchema,
+    async resolve({ input, ctx }) {
+      const decoded = decode(input.hash).split(':')
 
-    return true
-  },
-})
+      const [id, email] = decoded
+
+      const token = await ctx.prisma.loginToken.findFirst({
+        where: {
+          id,
+          user: {
+            email,
+          },
+        },
+        include: {
+          user: true,
+        },
+      })
+
+      if (!token) {
+        throw new trpc.TRPCError({
+          code: 'FORBIDDEN',
+          message: 'Invalid token',
+        })
+      }
+
+      const jwt = signJwt({
+        email: token.user.email,
+        id: token.user.id,
+      })
+
+      ctx.res.setHeader('Set-Cookie', serialize('token', jwt, { path: '/' }))
+
+      return {
+        redirect: token.redirect,
+      }
+    },
+  })
+  .query('me', {
+    resolve({ ctx }) {
+      return ctx.user
+    },
+  })
